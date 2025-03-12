4d3eb979d1293215ac0fa908e7d5fd124e5e43ce
Adnan Abdul Khaliq <adnanabdulkhaliq@outlook.com>
2025-03-12 02:02:05 +0000

    updated password_generation.py

--- backend/password_generation.py
+++ backend/password_generation.py
@@ -7,28 +7,26 @@ from string import ascii_letters, digits
 # string only includes letters and digits
 
 
-class GeneratePassword:
-    characters = ascii_letters + digits
+class PasswordGenerator:
 
-    @staticmethod
-    def __regular_generation():
+    @classmethod
+    def _characters(cls):
+        return ascii_letters + digits
+
+    @classmethod
+    def _regular_generation(cls):
         password = "-".join(
-            "".join(choice(GeneratePassword.characters) for i in range(6))
-            for i in range(3)
+            "".join(choice(cls._characters()) for i in range(6)) for i in range(3)
         )
 
         return password
 
-    @staticmethod
-    def __no_special_chars():
-        password = "".join(choice(GeneratePassword.characters) for i in range(15))
+    @classmethod
+    def _no_special_chars(cls):
+        password = "".join(choice(cls._characters()) for i in range(15))
 
         return password
 
-    @staticmethod
-    def generate(noSpecialChars=False):
-        return (
-            GeneratePassword.__no_special_chars()
-            if noSpecialChars
-            else GeneratePassword.__regular_generation()
-        )
+    @classmethod
+    def generate(cls, noSpecialChars=False):
+        return cls._no_special_chars() if noSpecialChars else cls._regular_generation()
